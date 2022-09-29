5aa1efcf64d4faed339765c07ccc17ec79c0acbd
rumahdevID <rumahpayment@gmail.com>
2022-09-29 11:37:10 +0700

    ResponseJson

---
+++ src/Halper/ResponseJson.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Ibnudirsan\LaraHandlerSanctum\Halper;
+
+class ResponseJson {
+
+    public static function Cretae()
+    {
+        # code...
+    }
+
+    public static function Read()
+    {
+        # code...
+    }
+
+    public function Show()
+    {
+        # code...
+    }
+
+    public static function update()
+    {
+        # code...
+    }
+
+    public static function Delete()
+    {
+        # code...
+    }
+
+}

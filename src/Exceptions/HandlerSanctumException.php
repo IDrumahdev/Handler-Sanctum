68345e2d6cd90fc91c025f2f8dc5c28552b0dbf3
rumahdevID <rumahpayment@gmail.com>
2022-10-02 16:29:15 +0700

    fixbug

--- src/Exceptions/HandlerSanctumException.php
+++ src/Exceptions/HandlerSanctumException.php
@@ -3,9 +3,10 @@
 namespace Ibnudirsan\HandlerSanctum\Exceptions;
 
 use Throwable;
-
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Foundation\Exceptions\Handler;
 use Ibnudirsan\HandlerSanctum\Halper\Exception\Response;
+use Ibnudirsan\HandlerSanctum\Halper\Response\ResponseLog;
 use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 
 class HandlerSanctumException extends Handler
@@ -33,16 +34,22 @@ class HandlerSanctumException extends Handler
      * @throws \Throwable
      */
     public function render($request, Throwable $exception) {
-        if ($request->is(config('HandlerSanctum.prefix.api'))) {
-            if ($exception instanceof HttpExceptionInterface) {
-                $statusCode = $exception->getStatusCode();
-                    return Response::Status($statusCode);
-            } elseif ($request->is(config('HandlerSanctum.prefix.api')) && auth('sanctum')->check() == false || empty($request->header('Authorization'))){
-                    return Response::Status(401);
-            } else {
-                $statusCode = 500;
-                    return Response::Status($statusCode);
-            }
+        if(!config('HandlerSanctum.prefix.api')) {
+            Artisan::call("vendor:publish --tag=sanctum-handler-ibnudirsan");
+            return ResponseLog::Success([
+                'message'   => 'Publishing [sanctum-handler-ibnudirsan] assets.',
+                'path'      => '[config/HandlerSanctum.php].'
+            ]);
+        }elseif($request->is(config('HandlerSanctum.prefix.api'))) {
+                if ($exception instanceof HttpExceptionInterface) {
+                    $statusCode = $exception->getStatusCode();
+                        return Response::Status($statusCode);
+                } elseif ($request->is(config('HandlerSanctum.prefix.api')) && auth('sanctum')->check() == false || empty($request->header('Authorization'))){
+                        return Response::Status(401);
+                } else {
+                    $statusCode = 500;
+                        return Response::Status($statusCode);
+                }
         }
             return parent::render($request, $exception);
     }

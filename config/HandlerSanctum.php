336319f6545374dc14f243a34ab26895373d3d2b
rumahdevID <rumahpayment@gmail.com>
2022-10-02 11:12:06 +0700

    fixbug

---
+++ config/HandlerSanctum.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    
+    'hidden' => [
+        'email_verified_at',
+        'created_at',
+        'updated_at'
+    ],
+
+    'prefix'  => [
+        'api'  => 'api/*'
+    ]
+];
